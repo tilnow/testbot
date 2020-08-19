aad42851aa22bee593a44af5189ab7470f58216d
tilnow <44685516+tilnow@users.noreply.github.com>
2020-08-20 00:23:44 +0300

    Update pythonfile.py
    try again with simple only part

--- pythonfile.py
+++ pythonfile.py
@@ -58,9 +58,10 @@ print(data)
 #    print("nothing did not work")
 #knack_app = knackpy.App(app_id=sys.argv[2],  api_key=sys.argv[3])
 try:
-    record4 = knack_app.record(method="update", data={'field_10':{'first':data["field_10"]["first"]+"w",'last':'b'}}, obj="object_2")
+    record4 = knack_app.record(method="update", data={'field_10':{'first':'x','last':'y'}}, obj="object_2")
     print("updating with nothing")
 except Exception as e:
+    print("hmm, got to exception")
     print(e)
     record4 = knack_app.record(method="create", data={'field_10':{'first':'a','last':'b'}}, obj="object_2")
     print("creating")
