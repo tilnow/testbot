3394841ca386723e85929b3c1d72df2ee0929585
tilnow <44685516+tilnow@users.noreply.github.com>
2020-08-19 20:04:12 +0300

    Update pythonfile.py

--- pythonfile.py
+++ pythonfile.py
@@ -1,1 +1,23 @@
+import sys
 print("this is a python file")
+import discord
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client),  client.guilds, client.guilds[0].text_channels)
+    for u in client.guilds[0].members:
+        print(u.name, u, [x.name for x in u.roles])
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+    if message.content.startswith('$die'):
+        exit(0)
+
+client.run(sys.argv[1])
